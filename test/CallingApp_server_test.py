28456d070a60e7d36eb66c0a30e30e7e0aa4b38e
Mikaela Marie Cashman <mcashman2@unl.edu>
2017-06-09 14:01:41 -0400

    testing imported modules

--- test/CallingApp_server_test.py
+++ test/CallingApp_server_test.py
@@ -4,6 +4,7 @@ import os  # noqa: F401
 import json  # noqa: F401
 import time
 import requests
+import shutil
 
 from os import environ
 try:
@@ -11,12 +12,14 @@ try:
 except:
     from configparser import ConfigParser  # py3
 
-from pprint import pprint  # noqa: F401
+from pprint import pprint, pformat  # noqa: F401
+
 
 from biokbase.workspace.client import Workspace as workspaceService
 from CallingApp.CallingAppImpl import CallingApp
 from CallingApp.CallingAppServer import MethodContext
-
+from fba_tools.fba_toolsClient import fba_tools
+from GenomeFileUtil.GenomeFileUtilClient import GenomeFileUtil
 
 class CallingAppTest(unittest.TestCase):
 
@@ -48,7 +51,65 @@ class CallingAppTest(unittest.TestCase):
         cls.serviceImpl = CallingApp(cls.cfg)
         cls.scratch = cls.cfg['scratch']
         cls.callback_url = os.environ['SDK_CALLBACK_URL']
+	##TESTING
+	print("Setting up files")
+	suffix = int(time.time() + 1000)
+	wsName = "test_Calling_" + str(suffix)
+	ret = cls.wsClient.create_workspace({'workspace':wsName})
+	fba = fba_tools(os.environ['SDK_CALLBACK_URL'])
+	genomeUtil = GenomeFileUtil(os.environ['SDK_CALLBACK_URL'])
+	
+	filenamegenome = "KBase_derived_BTheta.gbff"
+	pathgenome = os.path.join(cls.cfg['scratch'],filenamegenome)
+	shutil.copy(os.path.join("test_files",filenamegenome),pathgenome)
+	params = {'file':{'path':pathgenome},'genome_name':"BTheta",'workspace_name':wsName}
+	tempgenome = genomeUtil.genbank_to_genome(params)
+	print("Genome saved")
 
+	filename = "Jmmol.120.tsv"
+	path = os.path.join(cls.cfg['scratch'],filename)
+	shutil.copy(os.path.join("test_files",filename),path)
+	params = {'media_file':{'path':path},'media_name':'Jmmol120.media','workspace_name':wsName}
+	tempmedia=fba.tsv_file_to_media(params)	
+	print("Media saved")
+	
+	filenamemodel = "kb|g.436.fbamdl61.xls"
+	pathmodel = os.path.join(cls.cfg['scratch'],filenamemodel)
+	shutil.copy(os.path.join("test_files",filenamemodel),pathmodel)
+	params = {'model_file':{'path':pathmodel},'model_name':"BT127GF.FBAModel",'workspace_name':wsName,'genome':"BTheta",'biomass':["bio1"]}
+	tempmodel = fba.excel_file_to_model(params)
+	print("FBA Model saved")
+
+	#Actual Call
+	print("Setting up fba params")
+        #fbamodel_input = params.get('fbamodel_id')
+        #media_input = params.get('media')
+        #fbaOutput_input = params.get('fbaOutput_id')
+
+        fbainput = { 
+                'fbamodel_id': tempmodel['ref'],
+                'media_id': tempmedia['ref'],
+                'fba_output_id': "testOutinTest",
+                #'target_reaction': "bio1",
+                'workspace': wsName
+        }   
+        print(type(fbainput))
+        print(pformat(fbainput))
+        print("Calling FBA")
+        files = fba.run_flux_balance_analysis(fbainput)
+        print("FBA done, now finishing output")
+        print(files)
+        print("----attempting to download file as sbml")
+        #smbl_file = fba.model_to_sbml_file({'workspace_name': params['workspace'],'model_name': fbainput['fba_output_id']})
+        sbml_file = fba.export_fba_as_tsv_file({'input_ref': files['new_fba_ref']})
+	print(sbml_file)
+
+
+
+	#print("Ready to call")
+	#print(params)
+	#ret = cls.getImpl().CallingFBA(self.getContext(), {'workspace':wsName,'fbamodel_id':'BT127GF.FBAModel','media':tempmedia,'fbaOutput_id':'test.out'})
+        
     @classmethod
     def tearDownClass(cls):
         if hasattr(cls, 'wsName'):
@@ -80,7 +141,27 @@ class CallingAppTest(unittest.TestCase):
         #                                  'objects': []})
         #
         # Run your method by
-        ret = self.getImpl().CallingFBA(self.getContext(), {'workspace':'mikaelacashman:1487002527453','fbamodel_id':'BTheta.FBA','media':'Carbon-D-Glucose','fbaOutput_id':'test.out'})
+	pass
+	#print os.environ['HOME']
+	#fba = fba_tools(self.callback_url)
+	#Ranjan
+	#filename = "Jmmol.120.tsv"
+	#path = os.path.join(cls.cfg['scratch'],filename)
+	#shutil.copy(os.path.join("media",filename),path)
+
+	#print(self.callback_url)
+	#fba = fba_tools(os.environ['SDK_CALLBACK_URL'])
+	#for files in os.listdir("/kb/module/work/media"):
+	#	print(files)
+		#tempfile = "/kb/module/work/media/" + files
+		#print(tempfile)
+		#params = {'media_file':{'path':tempfile},'media_name':'Jmmol120.media','workspace_name':self.getWsName()}
+		#tempmedia=fba.tsv_file_to_media(params)
+	#params = {'media_file':{'path':path},'media_name':'Jmmol120.media'}
+	#os.path.join(
+	#params = {'media_file':{'path':"/kb/module/work/media/Jmmol.120.tsv"},'media_name':'Jmmol120.media','workspace_name':self.getWsName()}
+	#tempmedia=fba.tsv_file_to_media(params)
+        #ret = self.getImpl().CallingFBA(self.getContext(), {'workspace':'mikaelacashman:1487002527453','fbamodel_id':'BTheta.FBA','media':tempmedia,'fbaOutput_id':'test.out'})
         #
         # Check returned data with
         # self.assertEqual(ret[...], ...) or other unittest methods
